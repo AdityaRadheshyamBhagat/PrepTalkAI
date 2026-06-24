dce27dc57d0757d37fd8af3da94cd51cf7e02f38
AdityaRadheshyamBhagat <aditya.bhagat.skn.it@gmail.com>
2026-06-25 05:07:06 +0530

    Updated Firebase config to use env variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCpcdn1TZc4ALtMxAiWQHe4mrI4n820js0",
-  authDomain: "preptalkai-feffe.firebaseapp.com",
-  projectId: "preptalkai-feffe",
-  storageBucket: "preptalkai-feffe.firebasestorage.app",
-  messagingSenderId: "617253571815",
-  appId: "1:617253571815:web:5702c5273a8e0c00ed6642"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
