901ed5558ec2fa1f0695dd7f82e7b7d8870bd47e
AdityaRadheshyamBhagat <aditya.bhagat.skn.it@gmail.com>
2026-06-25 05:01:55 +0530

    Switch to new Firebase project

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxJAoER130YXE0o0OSZv13Ti3JcZIj4II",
-  authDomain: "preptalk-ai-1d79f.firebaseapp.com",
-  projectId: "preptalk-ai-1d79f",
-  storageBucket: "preptalk-ai-1d79f.firebasestorage.app",
-  messagingSenderId: "840866536761",
-  appId: "1:840866536761:web:9ccdb55974aa650c919ce8",
-  measurementId: "G-Z1PBJ60HCW",
+  apiKey: "AIzaSyCpcdn1TZc4ALtMxAiWQHe4mrI4n820js0",
+  authDomain: "preptalkai-feffe.firebaseapp.com",
+  projectId: "preptalkai-feffe",
+  storageBucket: "preptalkai-feffe.firebasestorage.app",
+  messagingSenderId: "617253571815",
+  appId: "1:617253571815:web:5702c5273a8e0c00ed6642"
 };
 
 const app = initializeApp(firebaseConfig);
